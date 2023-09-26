aa76117e98f1d2c72147c20ee77ad84074847bc9
infinitemax <m.brookman.byrne@gmail.com>
2023-09-26 10:57:04 +0100

    bring main up to date

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ console.log(test.encrypt("Mbz mx jwxv!", 0, 1, true))
 console.log(test.encrypt("Annie is the best!", 0, 1, false))
 
 const userText = document.getElementById("userText")
+const output = document.getElementById("output")
 
 let cipherText = userText.value;
 
-userText.addEventListener("keydown", (event) => {
+userText.addEventListener("input", (event) => {
 
     cipherText = userText.value;
     console.log(cipherText);
