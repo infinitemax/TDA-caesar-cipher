10fb25cb79ab69381bf4c0f4c9bc7ba09921597f
infinitemax <m.brookman.byrne@gmail.com>
2023-09-26 14:27:16 +0100

    add shift and increment sliders that change the encoding

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
-const test = new Cipher();
+const cipher = new Cipher();
 
-console.log(test.encrypt("Mbz mx jwxv!", 0, 1, true))
+console.log(`We can decrypt: ${cipher.encrypt("Mbz mx jwxv!", 0, 1, true)}`)
 
-console.log(test.encrypt("Annie is the best!", 0, 1, false))
+console.log(`We can encrypt: ${cipher.encrypt("Annie is the best!", 0, 1, false)}`)
 
 const userText = document.getElementById("userText")
 const output = document.getElementById("output")
 
 let cipherText = userText.value;
 
-userText.addEventListener("input", (event) => {
+
+// slider displays and values
+
+// shift
+let shift = 0;
+let shiftSlider = document.getElementById("shift");
+let shiftDisplay = document.getElementById("shiftDisplay")
+
+shiftSlider.oninput = () => {
+    shiftDisplay.innerHTML = shiftSlider.value;
+    shift = Number(shiftSlider.value);
+
+}
+
+// increment
+let increment = 0;
+let incrementSlider = document.getElementById("increment");
+let incrementDisplay = document.getElementById("incrementDisplay")
+
+incrementSlider.oninput = () => {
+    incrementDisplay.innerHTML = incrementSlider.value;
+    increment = Number(incrementSlider.value);
+}
+
+
+
+
+userText.addEventListener("input", () => {
 
     cipherText = userText.value;
-    console.log(cipherText);
+    let processedText = cipher.encrypt(cipherText, shift, increment, false)
+    
+
+
+    output.innerHTML = processedText;
 })
